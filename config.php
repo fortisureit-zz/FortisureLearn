c044e2d28d99e4d3bae2ee5d73a25531160bcc18
root <root@LEARN.icbahpfwbjcedjzdg0twdekiic.cx.internal.cloudapp.net>
2020-10-20 18:06:34 +0000

    Adding config file

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'fortisurelearn';
+$CFG->dbuser    = 'fitlearn';
+$CFG->dbpass    = 'f1tL3@rn';
+$CFG->prefix    = 'fl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = 'https://learn.fortisureit.com';
+$CFG->dataroot  = '/var/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
